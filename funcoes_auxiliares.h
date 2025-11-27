ba7a9117e57a6e0aa87f58d7df7491848d34f855
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2025-11-27 17:30:05 -0300

    fix

--- funcoes_auxiliares.h
+++ funcoes_auxiliares.h
@@ -8,5 +8,6 @@ int data_valida(int dia, int mes, int ano);
 int horario_valido(int hora, int minuto);
 void ordenar_eventos(struct Evento *vetor, int numeroEventos);
 int compara_eventos(struct Evento a, struct Evento b);
+void salvar_arquivo(struct Evento *vetor, int numeroEventos);
 
 #endif
